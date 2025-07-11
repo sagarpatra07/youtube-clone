27c5c64c798ebbf4a4f7028e4707d4fca29b849a
Sagar Patra <sagarpatra502@gmail.com>
2025-07-11 16:15:39 +0530

    utils add for error handling

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ dotenv.config({
 })
 
 connectDB()
+//Async method returns a Promise after completion
+.then(() => {
+    app.on("error", (error) => {
+        console.error('MongoDB connection error', error)
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})             
+.catch((error) => {
+    console.log('MongoDB connection failed', error);
+})
 
 
 
